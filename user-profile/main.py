76324144a189294c79923355a68df2c1cdf5ed6a
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-21 16:50:52 +0200

    Add user profile

--- user-profile/main.py
+++ user-profile/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+from UserProfile import get_profile
+from PyQt6.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
+from PyQt6.QtGui import QFont
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class MainWindow(QWidget):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self):
+        super().__init__()
+        self.profile = get_profile()
+
+
