6afcd34f5842d8d29a26884f810a351e6cc2d59f
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-21 16:46:49 +0200

    Add get profile method

--- user-profile/UserProfile.py
+++ user-profile/UserProfile.py
@@ -16,6 +16,20 @@ class UserProfile:
         self.__name = value
 
 
+def get_profile():
+    user_profile = UserProfile("images/david.jpg", "David Johnson")
+    user_profile.biography = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " \
+                              "Suspendisse nunc eros, faucibus non porta nec, interdum non mauris. " \
+                              "Phasellus posuere, mi quis faucibus elementum, tellus sem placerat neque, " \
+                              "non consequat massa ligula vel tortor."
+    user_profile.skills = "Python | C# | Java | C | C++"
+    user_profile.experience = [("Software Engineer", "Sed ultricies, dolor eu mattis molestie, felis libero "
+                                                     "vulputate diam, quis ultricies nulla massa in diam."),
+                               ("Systems Analyst", "Sed odio lorem, commodo ut dictum in, ullamcorper sit amet "
+                                                   "metus. Donec metus ipsum, fermentum ac ultricies ac, ultricies "
+                                                   "a diam.")]
+    return user_profile
+
 
 
 
